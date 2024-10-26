fda7416a23419a4929f33a7816916f7ed804ff7a
d7ry <7ianyun@berkeley.edu>
2024-10-26 03:33:43 -0700

    wip file picker

--- src/components/imgui_widgets/ImGuiWidgetTetraViewer.cpp
+++ src/components/imgui_widgets/ImGuiWidgetTetraViewer.cpp
@@ -1,20 +1,87 @@
+#include <filesystem>
+
+#include "Tetrium.h"
+
 #include "ImGuiWidgetTetraViewer.h"
 #include "imgui.h"
 
 const char* ImGuiWidgetTetraViewer::TETRA_IMAGE_FOLDER_PATH = "../assets/textures/tetra_images/";
 
+void ImGuiWidgetTetraViewer::drawTetraImage(
+    Tetrium* engine,
+    ColorSpace colorSpace,
+    const TetraImageFile& image
+)
+{
+    std::string imagePath = TETRA_IMAGE_FOLDER_PATH + image.paths[colorSpace];
+    ImGuiTexture tex = engine->getOrLoadImGuiTexture(engine->_imguiCtx, imagePath);
+
+    ImVec2 size = {(float)tex.width * _zoom, (float)tex.height * _zoom};
+    ImGui::Image(tex.id, size);
+}
+
 void ImGuiWidgetTetraViewer::Draw(Tetrium* engine, ColorSpace colorSpace)
 {
-    drawTetraImagePicker(colorSpace);
+
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 1));
+    ImGuiWindowFlags flags = 0;
+    if (_fullScreen) {
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+        flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse
+                | ImGuiWindowFlags_NoResize;
+    }
+
+    if (ImGui::Begin("Tetra Viewer", NULL, flags)) {
+        drawTetraImagePicker(colorSpace);
+
+        if (_currTetraImage != -1) {
+            drawTetraImage(engine, colorSpace, _tetraImages.at(_currTetraImage));
+        }
+    }
+    ImGui::End();
+    ImGui::PopStyleColor();
 }
 
 // reload all tetra images through IO
-void ImGuiWidgetTetraViewer::refreshTetraImagePicker() {
+void ImGuiWidgetTetraViewer::refreshTetraImagePicker()
+{
     _currTetraImage = -1;
     _tetraImages.clear();
-    // iterate over all files, each pairs of files that are in xxx_RGB and xxx_OCV format make up one tetra image
 
-
+    std::unordered_set<std::string> images;
+    // iterate over all files, each pairs of files that are in xxx_RGB.png and xxx_OCV.png format
+    // make up one tetra image
+    for (const std::filesystem::directory_entry& entry :
+         std::filesystem::directory_iterator(TETRA_IMAGE_FOLDER_PATH)) {
+        if (entry.is_directory() || entry.is_symlink()) {
+            continue;
+        }
+        std::string fileName = entry.path().filename().string();
+        bool isRGB = fileName.ends_with("_RGB.png");
+        bool isOCV = fileName.ends_with("_OCV.png");
+        if (!isRGB && !isOCV) {
+            continue;
+        }
+        // look for counterpart, if found, we have found a tetra image pair
+        std::string tetraImageName = fileName.substr(
+            0,
+            fileName.size() - (sizeof("_RGB.png") / sizeof(char)) + 1
+        ); // pure name of tetra image
+        std::string counterPart = tetraImageName;
+        if (isRGB) {
+            counterPart += "_OCV.png";
+        } else {
+            counterPart += "_RGB.png";
+        }
+        auto it = images.find(counterPart);
+        if (it != images.end()) { // found both part of RGB/OCV image pair -- create tetra image
+            TetraImageFile image{.name = tetraImageName, .paths = {fileName, counterPart}};
+            _tetraImages.emplace_back(image);
+        } else {
+            images.insert(fileName);
+        }
+    }
 }
 
 void ImGuiWidgetTetraViewer::drawTetraImagePicker(ColorSpace colorSpace)
@@ -23,7 +90,7 @@ void ImGuiWidgetTetraViewer::drawTetraImagePicker(ColorSpace colorSpace)
         refreshTetraImagePicker();
         _shouldRefreshFilePicker = false;
     }
-    if (ImGui::Button("Refresh") && colorSpace == RGB) {
+    if (ImGui::Button("Load Images") && colorSpace == RGB) {
         _shouldRefreshFilePicker = true;
     }
 
@@ -36,7 +103,6 @@ void ImGuiWidgetTetraViewer::drawTetraImagePicker(ColorSpace colorSpace)
             bool imageSelected = _currTetraImage == i;
             if (ImGui::Selectable(image.name.c_str(), imageSelected) && colorSpace == RGB) {
                 _currTetraImage = i;
-                INFO("Setting current tetra image to {}", _currTetraImage);
             }
         }
 
--- src/components/imgui_widgets/ImGuiWidgetTetraViewer.h
+++ src/components/imgui_widgets/ImGuiWidgetTetraViewer.h
@@ -2,27 +2,32 @@
 #include "ImGuiWidget.h"
 
 // image file browser + viewer for RGB/OCV images
-class ImGuiWidgetTetraViewer: public ImGuiWidgetMut
+class ImGuiWidgetTetraViewer : public ImGuiWidgetMut
 {
-    public:
-      virtual void Draw(Tetrium* engine, ColorSpace colorSpace) override;
+  public:
+    virtual void Draw(Tetrium* engine, ColorSpace colorSpace) override;
 
-    private:
-      struct TetraImageFile
-      {
-          std::string name;
-          std::string paths[ColorSpaceSize];
-      };
+  private:
+    struct TetraImageFile
+    {
+        std::string name;
+        std::string paths[ColorSpaceSize];
+    };
 
-      bool _shouldRefreshFilePicker = false;
+    bool _shouldRefreshFilePicker = false;
 
-      void refreshTetraImagePicker();
+    void refreshTetraImagePicker();
 
-      void drawTetraImagePicker(ColorSpace colorSpace);
+    void drawTetraImagePicker(ColorSpace colorSpace);
+    void drawTetraImage(Tetrium* engine, ColorSpace colorSpace, const TetraImageFile& image);
 
-      std::vector<TetraImageFile> _tetraImages;
+    std::vector<TetraImageFile> _tetraImages;
 
-      int _currTetraImage = -1;
-      
-      static const char* TETRA_IMAGE_FOLDER_PATH;
+    int _currTetraImage = -1;
+
+    static const char* TETRA_IMAGE_FOLDER_PATH;
+
+    // scaling params
+    bool _fullScreen = false;
+    float _zoom = 1.f;
 };
