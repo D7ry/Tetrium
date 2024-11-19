6c152a79d24287dfde32442f124ae5c6b04c5514
dtry <7ianyun@berkeley.edu>
2024-11-18 20:00:37 -0800

    update TetriumColor integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "Tetrium.h"
-
+#include "TetriumColor/TetriumColor.h"
 void printGreetingBanner()
 {
     // cool banner
@@ -10,30 +10,35 @@ void printGreetingBanner()
     std::cout << asciiLine << std::endl;
     std::cout << DEFAULTS::Engine::BANNER_TEXT << std::endl;
     std::cout << asciiLine << std::endl;
-
 }
 
-#include "TetriumColor/PseudoIsochromaticPlateGenerator.h"
+
+
 int main(int argc, char** argv)
 {
-    PseudoIsochromaticPlateGenerator generator(
-        "data/transform_dirs.txt",
-        "data/pregenerated_filenames.txt",
-        100
-    );
+    // std::vector<std::string> transformDirs{
+    //     "../extern/TetriumColor/TetriumColor/Assets/ColorSpaceTransforms/Neitz_530_559-RGBO"
+    // };
+    // std::vector<std::string> pregeneratedMetamers{
+    //     "../extern/TetriumColor/TetriumColor/Assets/PreGeneratedMetamers/Neitz_530_559-RGBO.pkl"
+    // };
+    //
+    // PseudoIsochromaticPlateGenerator generator(transformDirs, pregeneratedMetamers, 8);
+    // generator.NewPlate("plate_RGB.png", "plate_OCV.png", 27);
 
-    exit(0);
     printGreetingBanner();
     INIT_LOGS();
     INFO("Logger initialized.");
     DEBUG("running in debug mode");
-    Tetrium::InitOptions options{
-        .tetraMode = Tetrium::TetraMode::kEvenOddSoftwareSync
-    };
+    TetriumColor::Init();
+
+    Tetrium::InitOptions options{.tetraMode = Tetrium::TetraMode::kEvenOddSoftwareSync};
     Tetrium engine;
     engine.Init(options);
     engine.Run();
     engine.Cleanup();
+
+    TetriumColor::Cleanup();
 
     return 0;
 }
