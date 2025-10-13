ae69b6596f129a79b38aed545c9fb5792293b119
d7ryROG <jess.lee@berkeley.edu>
2025-10-13 14:26:27 -0700

    update pathing for windows

--- src/Pathing.h
+++ src/Pathing.h
@@ -2,6 +2,7 @@
 // NOTE: following must be inlined to avoid static initialization order fiasco
 #if defined(WIN32)
 inline const std::string ASSETS_PATH = "../../assets/";
+inline const std::string TETRIUM_COLOR_PATH = "../../extern/TetriumColor/";
 #else
 inline const std::string ASSETS_PATH = "../assets/";
 inline const std::string TETRIUM_COLOR_PATH = "../extern/TetriumColor/";
